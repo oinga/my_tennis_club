de2c2ee9514951448e99d0e6a7f4e6782d33d15d
oinga <ortiv@programmer.net>
2024-11-18 12:32:42 -0500

    update: add project files

---
+++ my_tennis_club/members/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('admin/', admin.site.urls),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
+]
